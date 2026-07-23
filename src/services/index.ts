e5ef976b2a906fbe886dbc8a9afade8d857d2753
Lokesh Kumar <lokeshkumar@Lokesh-Kumar-B907s-MacBook-Air.local>
2026-07-23 17:38:32 +0530

    Added getarticlesdata query

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,21 +1,65 @@
 import { gql } from "@apollo/client";
 
-export const getArticleBySlug = gql`
-  query getArticleBySlug($slug: String!, $locale: I18NLocaleCode) {
-    articles(locale: $locale, filters: { Slug: { eq: $slug } }) {
+export const getArticleData = gql`
+  query getAllArticles(
+    $page: Int
+    $pageSize: Int
+    $locale: I18NLocaleCode
+    $sort: [String] = ["PublishDate:desc"]
+    $filters: ArticleFiltersInput
+  ) {
+    articles(
+      pagination: { page: $page, pageSize: $pageSize }
+      locale: $locale
+      sort: $sort
+      filters: $filters
+    ) {
+      meta {
+        pagination {
+          total
+          page
+          pageCount
+        }
+      }
       data {
         id
         attributes {
+          readTime
+          ExcludeFromSlider
           Title
           Slug
-          Description
-
+          ShortDes
           CoverImg {
             data {
               attributes {
                 url
                 alternativeText
                 caption
+              }
+            }
+          }
+          PublishDate
+          Video
+          VideoLink
+          LikeCount
+          article_categories {
+            data {
+              attributes {
+                Name
+              }
+            }
+          }
+          authors {
+            data {
+              attributes {
+                Name
+                ProfilePic {
+                  data {
+                    attributes {
+                      url
+                    }
+                  }
+                }
               }
             }
           }
