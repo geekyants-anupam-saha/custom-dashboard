582f0fd0ead7a69fb47285fd788dd6b0e6cc7929
Lokesh Kumar <lokeshkumar@Lokesh-Kumar-B907s-MacBook-Air.local>
2026-07-28 20:57:16 +0530

    Added few icons

--- src/components/icons/Clock.tsx
+++ src/components/icons/Clock.tsx
@@ -1,4 +1,4 @@
-const Clock = (props:any) => (
+const Clock = (props: any) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="25"
@@ -7,7 +7,7 @@ const Clock = (props:any) => (
     viewBox="0 0 20 20"
   >
     <path
-      fill="#FFF8FF"
+      fill={props.color || "#FFF8FF"}
       d="M9.6 0a9.6 9.6 0 1 1 0 19.2A9.6 9.6 0 0 1 9.6 0m0 1.2a8.4 8.4 0 1 0 0 16.8 8.4 8.4 0 0 0 0-16.8M9 3.6a.6.6 0 0 1 .59.492l.01.108v5.4h3a.6.6 0 0 1 .108 1.19l-.108.01H9a.6.6 0 0 1-.59-.492L8.4 10.2v-6a.6.6 0 0 1 .6-.6"
     ></path>
   </svg>
